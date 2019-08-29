845b4b85bb72c23ff1576eb69c9030ade04611ce
pacosta1982 <acosta.aleoli@gmail.com>
2019-08-29 09:36:57 -0400

    ajustes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -93,7 +93,7 @@ class HomeController extends Controller
             $reg->NroExpS='A';
             $reg->ExpDId=$key+1;
             $reg->ExpDPerCod=$value->postulante_id?$value->getPostulante->cedula:"";
-            $reg->ExpDPerNom=utf8_encode($nombre);
+            $reg->ExpDPerNom=utf8_decode($nombre);
             $reg->ExpDTel=$value->postulante_id?$value->getPostulante->mobile:"";
             $reg->ExpDNivel=ProjectHasPostulantes::getNivel($value->postulante_id);
             $reg->ExpDFec= date_format($date, 'Y-d-m H:i:s');
