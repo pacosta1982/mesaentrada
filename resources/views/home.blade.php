b1b1883265386e3c5e2ad47a9f0a406d490d76e4
pacosta1982 <acosta.aleoli@gmail.com>
2019-09-19 09:35:30 -0400

    ajsutes cedula

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -102,7 +102,7 @@
                 <tr>
                   <td>{{$key+1}}</td>
                   <td>{{ $post->postulante_id?$post->getPostulante->first_name:"" }} {{ $post->postulante_id?$post->getPostulante->last_name:"" }}</td>
-                  <td class="text-center">{{ number_format($post->postulante_id?$post->getPostulante->cedula:"",0,".",".") }} </td>
+                  <td class="text-center">{{ $post->postulante_id?$post->getPostulante->cedula:"" }} </td>
                   <td class="text-center">{{ number_format(App\Models\ProjectHasPostulantes::getIngreso($post->postulante_id),0,".",".") }} </td>
                   <td class="text-center">{{ App\Models\ProjectHasPostulantes::getNivel($post->postulante_id) }}</td>
                   <td class="text-center">{{ $post->getMembers->count() + 1 }}</td>
