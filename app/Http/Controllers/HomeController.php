6a17dbc49209170b898f7ce7870516a3d612da22
Iedimes <osemidei@gmail.com>
2025-05-13 10:41:39 -0300

    Se saca dondicion de fecha de expediente

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,7 +46,7 @@ class HomeController extends Controller
             //return $request;
             $expediente = SIG005::where('NroExp',$request->input('nro_exp'))
             ->where('NroExpS','A')
-            ->where('NroExpFch','>=','2019-09-02')
+            // ->where('NroExpFch','>=','2019-09-02')
             ->first();
 
             $project = Project::where('id',$request->input('project_id'))
